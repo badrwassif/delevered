e585fb8a442d98da1b486e79bcc169d33123c93b
vbdw <csmartgamers@gmail.com>
2024-10-26 18:50:30 +0100

    navbar

---
+++ nav.js
@@ -0,0 +1,26 @@
+const nav = document.getElementById('nav');
+nav.innerHTML = `
+    <div id="nav-container">
+        <p class="logo-nav">Dilevred</p>
+        <p class="delivering-to">Delivering to<span> Restorant</span></p>
+        <button>My Account</button>
+    </div>
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
